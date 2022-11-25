d521d5c70849301c4ca5cd1242fa5f69c513139f
Emanuel-Boaventura <emanuelbmatos321@gmail.com>
2022-11-25 11:19:13 -0400

    first commit and base project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import Router from './Router'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
+  )
+}
+
+export default App
